825c72b8b8bcc2bcabd36f604c056bd55f976d37
GABRIEL CAVALCANTE DOS SANTOS <25132825@dsn127m1242316.cfp127aluno.local>
2026-03-09 15:41:00 -0300

    envio concluido da atividade de contador de idade

--- app/src/main/java/com/example/idade/MainActivity.kt
+++ app/src/main/java/com/example/idade/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.idade
 
+import android.R
+import android.R.attr.bottom
 import android.R.attr.fontWeight
 import android.R.attr.letterSpacing
+import android.R.attr.onClick
 import android.R.attr.text
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,17 +15,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -53,22 +62,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun sobreIdade(modifier: Modifier = Modifier) {
+
+    var idade by remember {
+        mutableIntStateOf(0)
+    }
+
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Red),
+            .fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
             modifier = Modifier
-                .background(Color.Gray)
                 .padding(top = 50.dp)
 
         ){
             Text(
-                modifier = Modifier
-                    .background(Color.Green),
+                modifier = Modifier,
                 text = "Qual e a sua Idade",
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
@@ -80,37 +91,104 @@ fun sobreIdade(modifier: Modifier = Modifier) {
 
         Box(
             modifier = Modifier
-                .background(Color.Gray)
-                .padding(top = 20.dp),
-
-
+                .padding(top = 40.dp),
         ){
             Text(
-                modifier = Modifier
-                    .background(Color.Yellow),
-
+                modifier = Modifier,
                 text = "Aperte os botoes para informar a sua idade",
                 fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
+                fontSize = 20.sp
             )
         }
-    }
+        Box(
+            modifier = Modifier
 
-}
+                .height(60.dp)
+                .padding(top = 15.dp)
+                .width(60.dp),
+               contentAlignment = Alignment.Center
+        ){
+            Text(
+                modifier = Modifier,
+                text = "$idade",
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp
 
-@Composable
-fun acaoBotao(modifier: Modifier = Modifier) {
-
-    Column(
-        modifier = Modifier
-            .background(Color.Magenta),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-
-    ) {
-
-        var idade by remember {
-            mutableStateOf("")
+            )
         }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            horizontalArrangement = Arrangement.spacedBy(50.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(
+                onClick = {idade --},
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp)
+                        .paddingFromBaseline(bottom = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "-",
+                        fontSize = 30.sp,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Button(
+                onClick = {idade ++},
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp)
+                        .paddingFromBaseline(bottom = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "+",
+                        fontSize = 30.sp,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+
+            }
+
+        }
+        val status = when  (idade){
+            in 0 .. 17 -> "Menor"
+            in 18 .. 180 -> "Maior"
+            else -> if (idade == -1){
+                idade ++
+            }else{
+                idade --
+            }
+        }
+
+        Text(
+            modifier = Modifier
+                .padding(top = 30.dp),
+
+            text = "Voce é $status de idade",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+
+        )
     }
+
 }
+
 
